7e2805a2ed60ca3ce4d497fe9083bf2bf635bb8f
Server Admin <admin@server.com>
2025-08-09 21:38:19 +0300

    Fix vite config for domain

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     port: 3000,
+<<<<<<< Updated upstream:vite.config.js
     host: true
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
+=======
+    allowedHosts: ['24autoflow.ru', 'www.24autoflow.ru', '217.199.252.227']
+  },
+  preview: {
+    host: '0.0.0.0',
+    port: 3000,
+    allowedHosts: ['24autoflow.ru', 'www.24autoflow.ru', '217.199.252.227']
+>>>>>>> Stashed changes:frontend/vite.config.js
   }
 }) 
